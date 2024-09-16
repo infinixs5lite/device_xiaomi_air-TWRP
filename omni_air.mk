b926f96e8cc7e8ec5b71dd97b44ff41285c58ed6
infinixs5lite <kelvinchinedu97@gmail.com>
2024-09-16 15:46:41 +0100

    Update omni_air.mk

--- omni_air.mk
+++ omni_air.mk
@@ -1,29 +1,27 @@
 #
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from air device
 $(call inherit-product, device/xiaomi/air/device.mk)
 
 PRODUCT_DEVICE := air
-PRODUCT_NAME := omni_air
+PRODUCT_NAME := twrp_air
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := air
 PRODUCT_MANUFACTURER := xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="miodm_air_global-user 13 TP1A.220624.014 V14.0.3.0.TGQMIXM release-keys"
-
-BUILD_FINGERPRINT := Redmi/air/air:13/TP1A.220624.014/V14.0.3.0.TGQMIXM:user/release-keys
